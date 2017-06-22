74a7db2dc0e24e3a96189182461c0cb5fb339c80
Gabriel Muniz <gabrielm854@fb.com>
2017-06-22 16:18:29 -0700

    Added tab bar

--- Flix App/NowPlayingViewControlerViewController.swift
+++ Flix App/NowPlayingViewControlerViewController.swift
@@ -11,11 +11,11 @@ import AlamofireImage
 
 class NowPlayingViewControlerViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [[String: Any]] = []
     var refreshControl: UIRefreshControl!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +24,8 @@ class NowPlayingViewControlerViewController: UIViewController, UITableViewDataSo
         tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
         fetchNowPlayingMovies()
-        
     }
+    
     
     func didPullToRefresh(_ refreshControl: UIRefreshControl) {
         fetchNowPlayingMovies()
@@ -36,14 +36,10 @@ class NowPlayingViewControlerViewController: UIViewController, UITableViewDataSo
     func fetchNowPlayingMovies() {
         activityIndicator.startAnimating()
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        
         let task = session.dataTask(with: request) { (data, respose, error) in
             //This will run when the network request returns
-            
             if let error = error {
                 print(error.localizedDescription)
             } else if let data = data {
@@ -57,27 +53,23 @@ class NowPlayingViewControlerViewController: UIViewController, UITableViewDataSo
         task.resume()
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
 
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        
-        
-        
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         let posterPathString = movie["poster_path"] as! String
         let baseURLString = "https://image.tmdb.org/t/p/w500"
-        
         let posterURL = URL(string: baseURLString + posterPathString)!
         cell.posterImageView.af_setImage(withURL: posterURL)
-        
         activityIndicator.stopAnimating()
         return cell
     }
@@ -92,11 +84,13 @@ class NowPlayingViewControlerViewController: UIViewController, UITableViewDataSo
         }
     }
     
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
+    
+    
     /*
     // MARK: - Navigation
 
